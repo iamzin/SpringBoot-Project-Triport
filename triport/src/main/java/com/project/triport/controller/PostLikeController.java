010af300ab01e5563daebba0d92e7984bca51889
beadoer1 <beadoer1@gmail.com>
2021-04-27 15:48:32 +0900

    Post 관련(Post, PostComment, PostLike) 뼈대(Controller, Service, Repository) 구성 완료(1차)

--- triport/src/main/java/com/project/triport/controller/PostLikeController.java
+++ triport/src/main/java/com/project/triport/controller/PostLikeController.java
@@ -1,8 +1,17 @@
 package com.project.triport.controller;
 
 
+import com.project.triport.responseDto.MsgResponseDto;
+import com.project.triport.service.PostLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class PostLikeController {
+    private final PostLikeService postLikeService;
+
+    @PostMapping("/api/posts/like/{postId}")
+    public MsgResponseDto creatDeletePostLike(){return postLikeService.creatDeletePostLike();}
 }
--- triport/src/main/java/com/project/triport/service/PostLikeService.java
+++ triport/src/main/java/com/project/triport/service/PostLikeService.java
@@ -1,4 +1,15 @@
 package com.project.triport.service;
 
+import com.project.triport.repository.PostLikeRepository;
+import com.project.triport.responseDto.MsgResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PostLikeService {
+
+    private final PostLikeRepository postLikeRepository;
+
+    public MsgResponseDto creatDeletePostLike(){return new MsgResponseDto(true, "좋아요 완료!");}
 }
