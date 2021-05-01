36e553f4039ad224c1994a42a2970bc59a1b2227
jinook <cowlsdnr77@gmail.com>
2021-05-01 15:35:58 +0900

    BasicBoard CommentResponseDto.java 수정사항 commit

--- triport/src/main/java/com/project/triport/responseDto/results/property/CommentResponseDto.java
+++ triport/src/main/java/com/project/triport/responseDto/results/property/CommentResponseDto.java
@@ -1,6 +1,6 @@
 package com.project.triport.responseDto.results.property;
 
-import com.project.triport.entity.BasicBoardComment;
+import com.project.triport.entity.BoardCommentParent;
 import com.project.triport.entity.PostComment;
 
 public class CommentResponseDto {
@@ -8,30 +8,12 @@ public class CommentResponseDto {
     private String nickname;
     private String profileImgUrl;
     private String contents;
-    // 밑에 지우고 리스트에 추가해야됨
-    private Boolean last;
-    private Long totalPage;
 
-    public CommentResponseDto(PostComment postComment){
-        this.id = postComment.getId();
-        this.nickname = postComment.getUser().getNickname();
-        this.profileImgUrl = postComment.getUser().getProfileImgUrl();
-        this.contents = postComment.getContents();
-    }
 
-    public CommentResponseDto(BasicBoardComment basicBoardComment){
-        this.id = basicBoardComment.getId();
-        this.nickname = basicBoardComment.getMember().getEmail(); //수정사항
-        this.profileImgUrl = basicBoardComment.getMember().getEmail(); // 수정사항
-        this.contents = basicBoardComment.getContents();
-    }
-
-    public CommentResponseDto(BasicBoardComment basicBoardComment, Boolean last, Long totalPage){
-        this.id = basicBoardComment.getId();
-        this.nickname = basicBoardComment.getMember().getEmail(); //수정사항
-        this.profileImgUrl = basicBoardComment.getMember().getEmail(); // 수정사항
-        this.contents = basicBoardComment.getContents();
-        this.last = last;
-        this.totalPage = totalPage;
+    public CommentResponseDto(BoardCommentParent boardCommentParent){
+        this.id = boardCommentParent.getId();
+        this.nickname = boardCommentParent.getMember().getNickname();
+        this.profileImgUrl = boardCommentParent.getMember().getProfileImgUrl();
+        this.contents = boardCommentParent.getContents();
     }
 }
