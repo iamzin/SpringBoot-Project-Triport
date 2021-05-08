a43257361c88a8d21331c51b86cd3233806cfaa6
eungenie <pkhykk@naver.com>
2021-05-08 10:33:26 +0900

    Create CorsConfig.java & Temporarily handle all access permission in SecurityConfig.java

---
+++ triport/src/main/java/com/project/triport/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.project.triport.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedOrigin("http://**");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
+    }
+}
