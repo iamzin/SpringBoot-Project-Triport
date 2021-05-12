2dbd6ba80f80d43571eb7388e831f6b13488e85f
beadoer1 <beadoer1@gmail.com>
2021-05-12 14:14:05 +0900

    video 인코딩 코덱 변경 (copy -> x264)

--- triport/src/main/java/com/project/triport/util/VideoFileUtil.java
+++ triport/src/main/java/com/project/triport/util/VideoFileUtil.java
@@ -67,17 +67,12 @@ public class VideoFileUtil {
                 .overrideOutputFiles(true) // Override the output if it exists
 
                 .addOutput(encodedDirectory + "/" + randomString + ".m3u8")   // Filename for the destination
-                .disableSubtitle()       // No subtiles
-
-                .setAudioChannels(1)         // Mono audio
-                .setAudioCodec("aac")        // using the aac codec
-                .setAudioSampleRate(48_000)  // at 48KHz
-                .setAudioBitRate(32768)      // at 32 kbit/s
-
-                .setVideoCodec("copy")     // Video using x264
-                .setVideoFrameRate(24, 1)     // at 24 frames per second
-
-                .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
+                .addExtraArgs("-start_number", "0", "-hls_time", "2", "-hls_list_size", "0")
+//                -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls
+                .setFormat("hls")
+                .disableSubtitle()           // No subtiles
+                .setVideoCodec("libx264")     // Video using x264
+//                .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
                 .done();
 
         FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
